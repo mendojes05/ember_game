9701ae898e4894b7d37b064d936733ddb5b00739
mendojes05 <mendozajessie05@gmail.com>
2025-10-02 23:56:44 -0700

    step 6

---
+++ lib/ember_quest.dart
@@ -0,0 +1,99 @@
+import 'package:flame/game.dart';
+import 'package:flame/components.dart';
+import 'actors/ember.dart';
+import 'actors/water_enemy.dart';
+import 'managers/segment_manager.dart';
+import 'objects/ground_block.dart';
+import 'objects/platform_block.dart';
+import 'objects/star.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/events.dart';
+import 'overlays/hud.dart';
+
+class EmberQuestGame extends FlameGame
+    with HasCollisionDetection, HasKeyboardHandlerComponents {  // EmberQuestGame();
+  late EmberPlayer _ember;
+  double objectSpeed = 0.0;
+  late double lastBlockXPosition = 0.0;
+  late UniqueKey lastBlockKey;
+  int starsCollected = 0;
+  int health = 3;
+
+  @override
+  Future<void> onLoad() async {
+    await images.loadAll([
+      'block.png',
+      'ember.png',
+      'ground.png',
+      'heart_half.png',
+      'heart.png',
+      'star.png',
+      'water_enemy.png',
+    ]);
+
+    // Everything in this tutorial assumes that the position
+    // of the `CameraComponent`s viewfinder (where the camera is looking)
+    // is in the top left corner, that's why we set the anchor here.
+    camera.viewfinder.anchor = Anchor.topLeft;
+    initializeGame();
+  }
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+
+  void loadGameSegments(int segmentIndex, double xPositionOffset) {
+    for (final block in segments[segmentIndex]) {
+      switch (block.blockType) {
+        case GroundBlock:
+          world.add(
+            GroundBlock(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
+        case PlatformBlock:
+          add(PlatformBlock(
+            gridPosition: block.gridPosition,
+            xOffset: xPositionOffset,
+          ));
+        case Star:
+          world.add(
+            Star(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
+        case WaterEnemy:
+          world.add(
+            WaterEnemy(
+              gridPosition: block.gridPosition,
+              xOffset: xPositionOffset,
+            ),
+          );
+      }
+    }
+  }
+
+  void initializeGame() {
+    // Assume that size.x < 3200
+    final segmentsToLoad = (size.x / 640).ceil();
+    segmentsToLoad.clamp(0, segments.length);
+
+    for (var i = 0; i <= segmentsToLoad; i++) {
+      loadGameSegments(i, (640 * i).toDouble());
+    }
+
+    _ember = EmberPlayer(
+      position: Vector2(128, canvasSize.y - 128),
+    );
+    world.add(_ember);
+    camera.viewport.add(Hud());
+  }
+
+
+}
+
+
+
